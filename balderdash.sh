290c020b127021802c97c14286fcf7806d2fc197
Neeraj Lamsal <itskop520@gmail.com>
2023-01-17 19:53:01 +0545

    fix: bug while reading existing file & add condtional checks

--- balderdash.sh
+++ balderdash.sh
@@ -14,11 +14,12 @@ fi
 PROFS_FILE="/tmp/profanity_list.txt"
 if [ -f $PROFS_FILE ]; then
   echo Using exiting $PROFS_FILE
-  PROFS=cat $PROFS_FILE
+  PROFS=$(cat $PROFS_FILE)
 else
   echo Writing the profanity list to $PROFS_FILE
   PARSED_LIST=$(curl -Ls https://raw.githubusercontent.com/chucknorris-io/swear-words/master/en | sed ':a;N;$!ba;s/\n/|/g')
   echo $PARSED_LIST > $PROFS_FILE
+  PROFS=$PARSED_LIST
 fi
 
 # This will find all the profanity in the current directory
@@ -33,13 +34,20 @@ fi
 # "Profanity found in file.txt"
 # 1: This is a line with red[profanity]
 # ```
+# echo $PROFS 
 function find_profanity_in_files() {
   echo Running the profanity filter in $1
   for file in $(find $1 -follow); do
-    echo Searching for profanity in $file ...
-    cat $file | grep -Ewn --color=always "($PROFS)"
+    if [[ $file == *".git"* || $file == "." ]]; then
+      continue
+    fi
+    # echo Searching for profanity in $file ...
+    cat $file | grep -Ewin --color=always "($PROFS)"
+    # cat $file | grep --color=always -Ewin "f.*u.*c.*k"
   done
 }
 
-# This is the main entrypoint for the script
 find_profanity_in_files $DIR $PROFS
+
+# This is the main entrypoint for the script
+# find_profanity_in_files $DIR $PROFS
